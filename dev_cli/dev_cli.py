b89f9427712fa9b079a48b9f42647b4ec431ab2e
arsalan arref <arsalan.dev.engineer@gmail.com>
2025-04-10 02:39:35 +0100

    adding toolkit sub-group to cli main group

--- dev_cli/dev_cli.py
+++ dev_cli/dev_cli.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
-# import libraries
+# =============== IMPORTING MODULES
+
+from commands.docker import cleanup
+from commands.aws import s3
 import click
 import sys
 from pathlib import Path
@@ -12,17 +15,7 @@ parentdir = currentdir.parent
 # add parent directory to sys.path
 sys.path.insert(0, str(parentdir))
 
-# =============== IMPORTING DOCKER MODULES
-
-# import modules from commands.docker
-from commands.docker import cleanup
-
-# =============== IMPORTING AWS MODULES
-
-# import modules from commands.aws
-from commands.aws import s3
-
-# =============== CLI GROUP
+# =============== CLI MAIN GROUP
 
 # define main command group for the cli tool
 @click.group(help="dev-cli tool: a command-line interface for various devops utilities.")
@@ -32,31 +25,39 @@ def cli():
     # is REQUIRED for defining the command group
     pass
 
-# =============== DOCKER SUB-GROUP
-
-# define docker command group
-@click.group(help="Commands for automating docker operations.")
-def docker():
-    pass
-
-# add commands to docker group
-docker.add_command(cleanup.cleanup)
-
-# =============== AWS SUB-GROUP
+# =============== SUB-GROUPS
 
 # define aws command group
 @click.group(help="Commands for automating aws operations.")
 def aws():
     pass
 
-# add commands to docker group
+# define docker command group
+@click.group(help="Commands for automating Docker container operations.")
+def docker():
+    pass
+
+# define docker command group
+@click.group(help="Commands for automating DevOps operations.")
+def toolkit():
+    pass
+
+# =============== ADD COMMANDS TO SUB-GROUPS
+
+# aws
 aws.add_command(s3.s3)
+
+# docker
+docker.add_command(cleanup.cleanup)
+
+# toolkit
 
 # =============== ADD SUB-GROUPS TO MAIN CLI GROUP
 
 # add sub-groups to the main cli group
-cli.add_command(docker)
 cli.add_command(aws)
+cli.add_command(docker)
+cli.add_command(toolkit)
 
 # =============== SCRIPT ENTRYPOINT
 
